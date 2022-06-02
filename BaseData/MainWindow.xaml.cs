bbb3493c848a9e74ef9d88163afd71ac79cffcfd
Альмир Абдрасулов <abdrasulov.almir@mail.ru>
2022-06-02 17:15:18 +0600

    Добавьте файлы проекта.

---
+++ BaseData/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BaseData
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void t1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.LightGreen);
+
+            l1.Stroke = brush;
+        }
+    }
+}
